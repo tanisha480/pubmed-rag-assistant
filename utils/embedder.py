97b91027d145519b68c7d6f4158de437ccd89a50
tanisha480 <ertanishajsr@gmail.com>
2026-07-05 02:43:24 +0530

    Add text embedding function using SentenceTransformer

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,1 +1,7 @@
+from sentence_transformers import SentenceTransformer
 
+_model = SentenceTransformer("all-mpnet-base-v2")
+
+def embed(text: str):
+    """Turn text into a 768-dimensional vector."""
+    return _model.encode(text).tolist()
